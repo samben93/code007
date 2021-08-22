d448e98c32be68eefcafa89345a69fa688b63121
samben93 <sam.jangce@gmail.com>
2021-08-22 20:45:37 +0800

    first commit

---
+++ src/BigNumber.java
@@ -0,0 +1,6 @@
+public class BigNumber {
+    public static void main(String[] args) {
+        int bigNum = 999999;
+        long number = 9999999999L;
+    }
+}
---
+++ src/BooleanAndChar.java
@@ -0,0 +1,5 @@
+public class BooleanAndChar {
+    public static void main(String[] args){
+
+    }
+}
